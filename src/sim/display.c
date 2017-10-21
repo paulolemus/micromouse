d9ca1224be8e3d8926f9ab085a62b4f9f17893d6
Paulo Lemus <paulolemus14@gmail.com>
2017-10-20 21:21:40 -1000

    Added ncurses functions to display to see if link works

--- src/sim/display.c
+++ src/sim/display.c
@@ -3,6 +3,7 @@
  * Author: Paulo Lemus
  */
 
+#include <ncurses.h>
 #include <micromouse/core/maze/maze.h>
 #include <micromouse/sim/display.h>
 
@@ -10,7 +11,13 @@ void init_display(
     const unsigned int width,
     const unsigned int height
 ) {
-
+    initscr();
+    raw();
+    keypad(stdscr, TRUE);
+    noecho();
+    printw("Init_display!\n");
+    refresh();
+    getch();
 }
 
 void clear_display() {
@@ -18,11 +25,11 @@ void clear_display() {
 }
 
 void render() {
-
+    refresh();
 }
 
 void finish_display() {
-
+    endwin();
 }
 
 void put_mouse(
