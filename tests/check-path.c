47eac5eb625cd8c052db88b021e76123b8a3a333
Paulo Lemus <paulolemus14@gmail.com>
2017-10-21 01:59:27 -1000

    Updated path test

--- tests/check-path.c
+++ tests/check-path.c
@@ -24,6 +24,22 @@ END_TEST
 START_TEST(check_clear_path) {
     Path path;
     clear_path(&path);
+    for(unsigned i = 0; i < PATH_SIZE; ++i) {
+        ck_assert_uint_eq(path.coords[i].x, 0);
+        ck_assert_uint_eq(path.coords[i].y, 0);
+    }
+}
+END_TEST
+
+START_TEST(check_get_next_coord_succ) {
+    Path path;
+    clear_path(&path);
+    path.coords[0].x = 3;
+    path.coords[0].y = 4;
+
+    Coord coord = get_next_coord(&path);
+    ck_assert_uint_eq(coord.x, 3);
+    ck_assert_uint_eq(coord.y, 4);
 }
 END_TEST
 
@@ -41,7 +57,9 @@ Suite* maze_suite(void) {
      * Add tests here
      */
     tcase_add_test(tc_core, check_basic);
-    tcase_add_test(tc_core, check_create_coord);
+    tcase_add_test(tc_core, check_create_path);
+    tcase_add_test(tc_core, check_clear_path);
+    tcase_add_test(tc_core, check_get_next_coord_succ);
 
     suite_add_tcase(s, tc_core);
     return s;
